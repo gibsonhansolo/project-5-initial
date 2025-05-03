4b88d869eda7b22843165b4e3af00a2de17f5fb5
gibsonhansolo <gib2151201@maricopa.edu>
2025-05-03 01:52:48 -0700

    feat: implement gnome sort

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -3,9 +3,20 @@ package com.example.sorting;
 public class SortingUtility {
 
 
-    public static <T extends Comparable<T>> void gnomeSort(T[] data) {
+    public static <T extends Comparable<T>> void gnomeSort(T[] a) {
+        int pos = 0;
 
-        // TODO implement Gnome Sort here
+        while (pos < a.length) {
+            if (pos == 0 || (a[pos].compareTo(a[pos - 1]) >= 0)) {
+                pos = pos + 1;
+            } else {
+                swap(a, pos, pos - 1);
+                pos-=1;
+            }
+        }
+    }
+
+
     }
 
 
