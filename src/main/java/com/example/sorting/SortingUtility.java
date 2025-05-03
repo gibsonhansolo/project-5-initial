8a852e6a2887f594486ea00b40740cca4e77291f
gibsonhansolo <gib2151201@maricopa.edu>
2025-05-03 01:53:55 -0700

    feat: implement cocktail shaker sort

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -17,13 +17,36 @@ public class SortingUtility {
     }
 
 
+    public static <T extends Comparable<T>> void cocktailShakerSort(T[] A) {
+        boolean swapped;
+        do {
+            swapped = false;
+            // Forward pass (left to right)
+            for (int i = 0; i < A.length - 1; i++) {
+                if (A[i].compareTo(A[i + 1]) > 0) {
+                    swap(A, i, i + 1);
+                    swapped = true;
+                }
+            }
+
+            // If no swaps occurred, array is sorted
+            if (!swapped) {
+                break;
+            }
+
+            swapped = false;
+            // Backward pass (right to left)
+            for (int i = A.length - 2; i >= 0; i--) {
+                if (A[i].compareTo(A[i + 1]) > 0) {
+                    swap(A, i, i + 1);
+                    swapped = true;
+                }
+            }
+        } while (swapped); // Continue until no swaps are made
     }
 
 
-    public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
 
-        // TODO implement Cocktail Shaker Sort here
-    }
 
 
     public static <T extends Comparable<T>> void shellSort(T[] data) {
